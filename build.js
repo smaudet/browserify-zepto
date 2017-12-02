c23791cca2c8606debefbd20aed706eb1af7c8ca
smaudet <smaudet@sebastianaudet.com>
2017-12-02 14:31:49 -0500

    Bump version to latest zepto

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var HTTP = require("q-io/http"),
     FS = require("q-io/fs");
 
-HTTP.read('https://raw.githubusercontent.com/peutetre/zepto/1.1.4/zepto.js')
+HTTP.read('https://raw.githubusercontent.com/peutetre/zepto/1.2.0/zepto.js')
     .then(function (body) {
         var rslt = body.toString()
                     .replace(/var Zepto =/, 'var Zepto = module.exports =')
